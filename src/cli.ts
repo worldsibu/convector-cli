c46da63540023013f246a147b7d6d1a934744ce1
Walter Montes <walmon93@hotmail.com>
2018-11-11 16:21:35 -0600

    google analytics integration with consent first
    Signed-off-by: Walter Montes <walmon93@hotmail.com>

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,8 @@ import { SysWrapper } from './utils/sysWrapper';
 import { join } from 'path';
 import { PackageStructureCompiler, ProjectStructureCompiler } from './utils';
 import { ModelModel, ControllerModel } from './models';
+import { Analytics } from './utils/analytics';
+import * as Insight from 'insight';
 
 export class CLI {
     static async create(name: string, chaincode?: string) {
@@ -26,31 +28,39 @@ export class CLI {
         return cli;
     }
 
+    analytics: Analytics;
+
     constructor(public name?: string, public chaincode?: string) {
+        this.analytics = new Analytics();
     }
 
     public async init() {
         let structure = new ProjectStructureCompiler(this.name);
         await structure.save();
+        this.analytics.trackNewProject(`PROJECT=${this.name}`);
         if (this.chaincode) {
             let packageStructure = new PackageStructureCompiler(this.chaincode, this.name);
             await packageStructure.save();
+            this.analytics.trackGenerateCC(`CHAINCODE=${this.chaincode}`);
         }
     }
 
     public async generateCC() {
         let packageStructure = new PackageStructureCompiler(this.chaincode, this.name);
         await packageStructure.save();
+        this.analytics.trackGenerateCC(`CHAINCODE=${this.chaincode}`);
     }
 
     public async generateModel() {
         let model = new ModelModel(this.chaincode, this.name, true);
         await model.save();
+        this.analytics.trackGenerateModel(`CHAINCODE=${this.chaincode}`);
     }
 
     public async generateController() {
         let ctrl = new ControllerModel(this.chaincode, this.name, true);
         await ctrl.save();
+        this.analytics.trackGenerateController(`CHAINCODE=${this.chaincode}`);
     }
 
 }
